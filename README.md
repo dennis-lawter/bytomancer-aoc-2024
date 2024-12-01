5eb7d8bb478f2a62f65ae36a361ee8d7d8397261
Bytomancer <bytomancer@gmail.com>
2024-12-01 00:16:34 -0500

    day 1 in rust

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bytomancer-aoc-2024
 My solution repository for Advent of Code 2024
+
+## Personal Times
+      -------Part 1--------   -------Part 2--------
+Day       Time  Rank  Score       Time  Rank  Score
+  1   00:08:06  3540      0   00:15:01  4195      0
--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,25 +1,87 @@
+use std::collections::HashMap;
+
 use super::final_answer;
 use super::input_raw;
 
 const DAY: u8 = 01;
 
-async fn input(example: bool) -> Vec<String> {
+async fn input(example: bool) -> Vec<(u64, u64)> {
+    let mut output = vec![];
     let raw = input_raw(DAY, example).await;
-    let lines = raw
+    let lines: Vec<String> = raw
         .lines()
         .map(|item| item.to_owned())
         .filter(|item| item.len() > 0)
         .collect();
 
-    lines
+    for line in lines {
+        let stripped: Vec<String> = line
+            .split_ascii_whitespace()
+            .map(|item| item.to_owned())
+            .collect();
+        let mut res = (0u64, 0u64);
+        res.0 = stripped[0].parse().unwrap();
+        res.1 = stripped[1].parse().unwrap();
+        output.push(res);
+    }
+
+    output
 }
 
 pub async fn d01s1(submit: bool, example: bool) {
     let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, 1).await;
+
+    let mut left: Vec<u64> = vec![];
+    let mut right: Vec<u64> = vec![];
+
+    for line in input {
+        left.push(line.0);
+        right.push(line.1);
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut ans = 0;
+
+    for i in 0..left.len() {
+        if left[i] < right[i] {
+            ans += right[i] - left[i];
+        } else {
+            ans += left[i] - right[i];
+        }
+    }
+
+    final_answer(ans, submit, DAY, 1).await;
 }
 
 pub async fn d01s2(submit: bool, example: bool) {
     let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, 2).await;
+    let mut left: Vec<u64> = vec![];
+    let mut right: Vec<u64> = vec![];
+
+    for line in input {
+        left.push(line.0);
+        right.push(line.1);
+    }
+    let mut r_occ: HashMap<u64, u64> = HashMap::new();
+    for r in right {
+        if !r_occ.contains_key(&r) {
+            r_occ.insert(r, 1);
+        } else {
+            let old = r_occ.get(&r).unwrap();
+            r_occ.insert(r, old + 1);
+        }
+    }
+    let mut ans = 0;
+    for l in left {
+        let occ = r_occ.get(&l);
+        match occ {
+            Some(o) => {
+                ans += o * l;
+            }
+            None => {}
+        }
+    }
+    final_answer(ans, submit, DAY, 2).await;
 }
